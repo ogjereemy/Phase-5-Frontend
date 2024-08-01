52e39d9fae604c31966ff6b7fafb23ea0fc9ca50
ogola <ogolajeremy601@gmail.com>
2024-08-01 13:53:47 +0300

    ft bg

--- App.jsx
+++ App.jsx
@@ -1,18 +1,19 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import Signup from './components/auth/Signup';
-import Login from './components/auth/Login';
-import UserDashboard from './components/dashboard/UserDashboard';
-import UserProfile from './components/dashboard/UserProfile';
-import CoachDashboard from './components/dashboard/CoachDashboard';
-import Overview from './components/Overview';
-import WorkoutList from './components/WorkoutList';
-import NutritionLogs from './components/dashboard/NutritionLogs';
-import ProgressCharts from './components/dashboard/ProgressCharts';
-import PasswordReset from './components/auth/PasswordReset';
-import { AuthProvider } from './context/AuthContext';
+import Home from './src/components/Home';
+import Signup from './src/components/auth/Signup';
+import Login from './src/components/auth/Login';
+import UserDashboard from './src/components/dashboard/UserDashboard';
+import UserProfile from './src/components/dashboard/UserProfile';
+import CoachDashboard from './src/components/dashboard/CoachDashboard';
+import Overview from './src/components/Overview';
+import GoalForm from './src/pages/GoalForm';
+import WorkoutList from './src/components/WorkoutList';
+import NutritionLogs from './src/components/dashboard/NutritionLogs';
+import ProgressCharts from './src/components/dashboard/ProgressCharts';
+import PasswordReset from './src/components/auth/PasswordReset';
+import { AuthProvider } from './src/context/AuthContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -28,6 +29,7 @@ function App() {
                     <Route path='/workout' element={<WorkoutList/>}/>
                     <Route path="/diet-plan" element={<NutritionLogs />} />
                     <Route path="/progress" element={<ProgressCharts />} />
+                    <Route path='/goals' element ={<GoalForm />}  />
                     <Route path="/user-dashboard" element={<UserDashboard />} />
                     <Route path="/user-profile" element={<UserProfile />} />
                     <Route path="/coach-dashboard" element={<CoachDashboard />} />
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from '../App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 
@@ -9,6 +9,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
     <App />
     </BrowserRouter>
-   
   </React.StrictMode>,
 )
