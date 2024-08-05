68434ece0197c03358e9190848aa9b869c6a5e12
tyranorah <tashnorah@gmail.com>
2024-08-05 11:32:03 +0300

    fixingconflict

--- src/components/Achievement.jsx
+++ src/components/Achievement.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import '../Achievement.css';
+import '../Achievement.css'
 
-function Achievements({ achievements }) {
+function Achievements() {
   return (
     <div className="achievements">
-      {achievements.map((achievement, index) => (
-        <div key={index} className="achievement">
-          <h4>{achievement.value}</h4>
-          <p>{achievement.label}</p>
-        </div>
-      ))}
+      <div className="achievement">
+        <h4>15</h4>
+        <p>hours</p>
+      </div>
+      <div className="achievement">
+        <h4>550</h4>
+        <p>Kcal</p>
+      </div>
+      <div className="achievement">
+        <h4>15</h4>
+        <p>Poses</p>
+      </div>
+      <div className="achievement">
+        <h4>5</h4>
+        <p>Sets</p>
+      </div>
+      <div className="achievement">
+        <h4>5</h4>
+        <p>Sets</p>
+      </div>
     </div>
   );
 }
